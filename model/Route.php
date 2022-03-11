e11a49ebd4346127f89b25bb3873d72025e7f19f
metaxiii <lehchibi.gael@pm.me>
2022-03-11 17:56:18 +0100

    remove not working, this finally worked

--- model/Route.php
+++ model/Route.php
@@ -30,7 +30,7 @@
         public function call() {
             if (is_string($this->callable)) {
                 $params = explode('#', $this->callable);
-                $controller = "\\metaxiii\\blog\\" . $params[0] . "Controller";
+                $controller = $params[0] . "Controller";
                 $controller = new $controller();
                 return call_user_func_array([$controller, $params[1]], $this->matches);
             } else {
